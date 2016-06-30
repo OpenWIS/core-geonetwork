0ece9689ae1e0a666a07256875536c58c2b81eb0
Andy Jacobs <andy.jacobs@scisys.co.uk>
2016-06-30 09:56:45 +0100

    Add data services to Vagrant all-in-one box

--- openwis-deployment/vagrant-allinone/provisioning/manifests/allinone.pp
+++ openwis-deployment/vagrant-allinone/provisioning/manifests/allinone.pp
@@ -2,10 +2,14 @@ Package {
 	allow_virtual => false,
 }
 
+# Apache proxy has no hard dependencies
 class { openwis::apache_proxy:
 }
 
+# database, data services & portal must be provisioned in correct order
 class { openwis::database:
+} ->
+class { openwis::data_services:
 } ->
 class { openwis::portal:
 }
